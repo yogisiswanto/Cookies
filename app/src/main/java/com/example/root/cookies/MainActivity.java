99537a83d27e9337eed8ee47ea90ca87e17a97a6
yogisiswanto <yogisiswanto.c2@gmail.com>
2017-11-01 16:22:41 +0700

    initial

---
+++ app/src/main/java/com/example/root/cookies/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.root.cookies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // Find a reference to the ImageView in the layout. Change the image.
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.download1);
+
+        // Find a reference to the TextView in the layout. Change the text.
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
+    }
+}
